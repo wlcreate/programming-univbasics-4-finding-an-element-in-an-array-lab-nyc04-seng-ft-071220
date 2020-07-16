270119c3d3c5dbc65423b9642595ddf6c87b3d67
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 21:50:46 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   count = 0
   found_value_index = nil
   while count < array.length do
-    if array[count] == value to find
+    if array[count] == value_to_find
       found_value_index = count
     end
     count += 1

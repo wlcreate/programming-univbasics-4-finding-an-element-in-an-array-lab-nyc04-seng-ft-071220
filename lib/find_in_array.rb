65eb3206c4ac3db75a5fe98cfff48902a6f4816e
Waverley Leung <waverley.nyc@gmail.com>
2020-07-16 21:41:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  while array.include?(value_to_find) do
+    #return the index of value_to_find
+    #indexes are element - 1
+    array.index(value_to_find)
+  end
 end
